4d5408ea28c79df2ea527e1594460523842c2066
karma Tsheten <kartendruk@gmail.com>
2018-08-22 18:26:42 +0700

    Initial Commit

---
+++ Map route sample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Map route sample
+//
+//  Created by Nando Septian Husni on 22/08/18.
+//  Copyright © 2018 IMASTUDIO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
