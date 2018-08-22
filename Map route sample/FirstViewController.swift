29ac67aa00066d61229c62065e4bc1afea629630
karma Tsheten <kartendruk@gmail.com>
2018-08-22 20:30:15 +0700

    inisial

--- Map route sample/FirstViewController.swift
+++ Map route sample/FirstViewController.swift
@@ -7,17 +7,98 @@
 //
 
 import UIKit
+import GoogleMaps
+import MapKit
+import Alamofire
+import SwiftyJSON
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,CLLocationManagerDelegate {
+    
+    let coordinat  = CLLocationManager()
+    
+    var mapView : GMSMapView?
+    var lat : Double?
+    var lon : Double?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        coordinat.delegate = self
+        coordinat.requestLocation()
+        coordinat.requestAlwaysAuthorization()
+        
+        
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        view = mapView
+//
+//        // Creates a marker in the center of the map.
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+       
     }
 
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print(error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let coordinat = locations.last?.coordinate
+        lat = coordinat?.latitude
+        lon = coordinat?.longitude
+        let camera = GMSCameraPosition.camera(withLatitude: lat!, longitude: lon!, zoom: 17.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: lat!, longitude: lon!)
+        marker.title = "title"
+        marker.snippet = "lokasi ku "
+        marker.map = mapView
+        
+        route()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func route(){
+        
+        let posisi1 = String(lat!) + "," + String(lon!)
+        let posisi2 = "-6.1935488,106.7887633"
+        
+        let url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + posisi1 + "&destination=" + posisi2
+        
+        print(url)
+        
+        Alamofire.request(url).responseJSON { (json) in
+            
+            let json2 = JSON(json.result.value as Any)
+            let route = json2["routes"].arrayValue
+            let poli = route[0]["overview_polyline"].dictionaryValue
+            
+            let point = poli["points"]?.stringValue
+            
+            let path = GMSPath(fromEncodedPath: point!)
+            let polyi = GMSPolyline(path: path)
+            
+            polyi.strokeColor  = UIColor.black
+            polyi.strokeWidth =   5
+            polyi.map = self.mapView
+            
+            
+            
+        }
+        
     }
 
 
--- Map route sample/SecondViewController.swift
+++ Map route sample/SecondViewController.swift
@@ -7,17 +7,89 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,MKMapViewDelegate {
 
+    @IBOutlet weak var mapview: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapview.delegate = self
+        
+        
+        // 2.
+        let sourceLocation = CLLocationCoordinate2D(latitude: 40.759011, longitude: -73.984472)
+        let destinationLocation = CLLocationCoordinate2D(latitude: 40.748441, longitude: -73.985564)
+        
+        // 3.
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+        
+        // 4.
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        
+        // 5.
+        let sourceAnnotation = MKPointAnnotation()
+        sourceAnnotation.title = "Times Square"
+        
+        if let location = sourcePlacemark.location {
+            sourceAnnotation.coordinate = location.coordinate
+        }
+        
+        
+        let destinationAnnotation = MKPointAnnotation()
+        destinationAnnotation.title = "Empire State Building"
+        
+        if let location = destinationPlacemark.location {
+            destinationAnnotation.coordinate = location.coordinate
+        }
+        
+        // 6.
+        self.mapview.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+        
+        // 7.
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .automobile
+        
+        // Calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        
+        // 8.
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                
+                return
+            }
+            
+            
+            let route = response.routes[0]
+            self.mapview.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+            
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.red
+        renderer.lineWidth = 4.0
+        
+        return renderer
     }
 
 
